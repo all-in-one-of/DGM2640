89a0389a11143584a2c472961f4f844765c759ef
Unknown <skyesladeut@outlook.com>
2019-02-28 15:53:44 -0700

    giraffe

---
+++ Unity/InClassScripts/Assets/Scripts/CharacterMovement/AnimationTriggers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class AnimationTriggers : MonoBehaviour
+{
+  private Animator anim;
+
+  private void Start()
+  {
+    anim = GetComponent<Animator>();
+  }
+
+  private void Update()
+  {
+    if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+    {
+      anim.SetTrigger("Walk");
+    }
+    else
+    {
+      anim.SetTrigger("Stop");
+    }
+  }
+}
