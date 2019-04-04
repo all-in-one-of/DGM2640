b2e876063b2c4d0f19d072b1c695ac53186eaa60
Unknown <skyesladeut@outlook.com>
2019-04-03 22:48:51 -0600

    Working on new rig

---
+++ Unity/ClassScripts/Assets/Scripts/UpMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpMovement : MonoBehaviour
+{
+    public float movementspeed;
+    
+    private void Update()
+    {
+        transform.Translate(Vector3.up*movementspeed*Time.deltaTime);
+    }
+}
