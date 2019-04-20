e176edd5862618718550dc2412f631560b5a1c2d
Unknown <skyesladeut@outlook.com>
2019-04-20 13:15:19 -0600

    Finished Putting Giraffe into Unity

---
+++ FullBackGround/Assets/Giraffe/Giraffe.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Giraffe : MonoBehaviour
+{
+
+	public GameObject Muscles, Skeletons, Organs, Skin;
+
+	public void SetSkin()
+	{
+		Muscles.SetActive(false);
+		Skeletons.SetActive(false);
+		Organs.SetActive(false);
+		Skin.SetActive(true);
+	}
+	public void SetMuscles()
+	{
+		Muscles.SetActive(true);
+		Skeletons.SetActive(true);
+		Organs.SetActive(true);
+		Skin.SetActive(false);
+	}
+	public void SetSkeleton()
+	{
+		Muscles.SetActive(false);
+		Skeletons.SetActive(true);
+		Organs.SetActive(true);
+		Skin.SetActive(false);
+	}
+	public void SetOrgans()
+	{
+		Muscles.SetActive(false);
+		Skeletons.SetActive(false);
+		Organs.SetActive(true);
+		Skin.SetActive(false);
+	}
+}
---
+++ FullBackGround/Assets/Giraffe/GiraffeAnim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiraffeAnim : MonoBehaviour
+{
+
+	private Animator Anim;
+
+	private void Start()
+	{
+		Anim = GetComponent<Animator>();
+	}
+
+	public void SetIdle()
+	{
+		Anim.SetBool("Pose", false);
+		Anim.SetBool("Idle", true);
+		
+	}
+
+	public void SetPose()
+	{
+		Anim.SetBool("Idle", false);
+		Anim.SetBool("Pose", true);
+		
+	}
+}
