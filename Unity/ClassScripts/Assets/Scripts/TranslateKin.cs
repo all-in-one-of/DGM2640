b0ce044cf67ce816e0d1ac9b60375a408b76eed2
Unknown <skyesladeut@outlook.com>
2019-05-01 16:55:12 -0600

    Dragon Fairy

---
+++ Unity/ClassScripts/Assets/Scripts/TranslateKin.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateKin : MonoBehaviour
+{
+    private float ZipTime = .01f;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    public GameObject endZip;
+    public GameObject Player;
+    //public Rigidbody Rb;
+    public GameObject StartZip;
+    private float speedscale;
+    //public CharacterMovement Moving;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Zipline"))
+        {
+            //Rb.useGravity = false;
+            //Moving.characterSpeed = 0;
+            startPos = StartZip.transform.position;
+            endPos = endZip.transform.position;
+            StartCoroutine(_Moveplayer());
+            speedscale = 1;
+        }
+    }
+
+    IEnumerator _Moveplayer()
+    {
+        while (Player.transform.position.z < (endZip.transform.position.z -.1f))
+        {
+            startPos = Player.transform.position;
+            Player.transform.position = Vector3.Lerp(startPos, endPos, ZipTime*Time.deltaTime*speedscale);
+            print(ZipTime*Time.deltaTime*speedscale);
+            speedscale += Time.deltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
